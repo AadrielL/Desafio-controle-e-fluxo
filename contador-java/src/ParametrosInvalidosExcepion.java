5cad2e3fb5353c7e1a2f02cfc98dc8d01174cf17
adriel morais <adrie3lmorais@gmail.com>
2025-04-08 20:36:57 -0300

    projeto finalizado

---
+++ contador-java/src/ParametrosInvalidosExcepion.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosExcepion extends Exception {
+    public static void main(String[] args) {
+        System.out.println("o segundo parâmetro deve ser maior que o primeiro");
+    }
+    
+}
